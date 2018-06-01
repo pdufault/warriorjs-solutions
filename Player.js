40b57988d15bba45c3a8ea4dfa37fa437b8cc52a
Phil Dufault <phil@dufault.info>
2018-05-31 21:40:07 -0700

    feat: level 3

--- Player.js
+++ Player.js
@@ -1,9 +1,18 @@
 class Player {
+
+  amIHurt(warrior) {
+    return warrior.health() < 20
+  }
+
+  isSomeOneThere(warrior) {
+    return warrior.feel().isEmpty()
+  }
+
   playTurn(warrior) {
-    if (warrior.feel().isEmpty()) { 
-      warrior.walk()
-    } else {
+    if (this.isSomeoneThere()) {
       warrior.attack()
+    else if (this.amIHurt(warrior)) {
+      warrior.rest()
     }
   }
 }
